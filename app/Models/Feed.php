517f4128cb7690c2057a94777c865231818fe819
Alejandro Estarlich <hello@alejandroestarlich.es>
2021-06-01 16:51:26 +0200

    Modelo Feed y Controlador creado

---
+++ app/Models/Feed.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class feed extends Model
+{
+    use HasFactory;
+
+    // Relacionamos el modelo con la tabla
+    protected $table = 'news';
+
+    // Relación Muchos a uno con el user (Publisher cuando crea un artículo), relaciona un modelo con otro
+    public function user(){
+        return $this->belongsTo('App\Models\User', 'publisher');
+    }
+}
